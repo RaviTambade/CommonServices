8e1769d108ff6fb8604309f46d491e00d09e9af2
Akashajab1999 <118430799+Akashajab1999@users.noreply.github.com>
2023-07-02 12:15:46 +0530

    new changes in details method

--- PersonalInfoAPI/Models/People.cs
+++ PersonalInfoAPI/Models/People.cs
@@ -5,7 +5,7 @@ public class People{
     public int Id{get; set;}
     public string FirstName{get; set;}
     public string LastName{get; set;}
-    public DateOnly BirthDate{get; set;}
+    public DateTime BirthDate{get; set;}
     public string AadharId {get; set;}
     public string Gender {get; set;}
     public string Email {get; set;}
