ed446fa89dfb3cb7ecc77e0341bac6ce092c851e
Gorerohit19 <gore.rohit1902@gmail.com>
2023-07-02 16:25:28 +0530

    Deposit Component is added

--- FrontEnd/src/app/payment-gateway/account/account.component.ts
+++ FrontEnd/src/app/payment-gateway/account/account.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { PaymentGatewayService } from '../payment-gateway.service';
 
 @Component({
   selector: 'app-account',
@@ -6,5 +8,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./account.component.css']
 })
 export class AccountComponent {
+  transactionId:any;
+  successful:boolean=false;
+  failed:boolean=false;
+  credential:any={
+    "fromAcct":'',
+    "amount":0,
+    "fromIfsc":'',
+    "toAcct":'46556565566',
+     "toIfsc":'AXIS0000296',
+  }
+  constructor(private svc:PaymentGatewayService){}
 
+
+  onProceed(form:NgForm){
+    this.credential.fromAcct=form.value.acctno;
+    this.credential.amount=parseFloat(form.value.amount);
+    this.credential.fromIfsc=form.value.ifsc;
+    console.log(this.credential)
+    this.svc.fundTransfer(this.credential).subscribe((res)=>{
+      console.log(res)
+      this.transactionId=res;
+    })
+ 
+  }
 }
