2ebb9ea797589f0dc0a933781e2b6e0a146d3cb9
NavaleAbhay <navale.abhay12@gmail.com>
2023-07-06 11:57:42 +0530

    user module added

---
+++ UI/src/app/users/alluser/alluser.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-alluser',
+  templateUrl: './alluser.component.html',
+  styleUrls: ['./alluser.component.css']
+})
+export class AlluserComponent {
+
+}
---
+++ UI/src/app/users/getuser/getuser.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-getuser',
+  templateUrl: './getuser.component.html',
+  styleUrls: ['./getuser.component.css']
+})
+export class GetuserComponent {
+
+}
