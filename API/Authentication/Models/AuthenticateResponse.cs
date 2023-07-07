3c6ba6431ed9895904254da1d18fe709e70a7109
Akashajab1999 <118430799+Akashajab1999@users.noreply.github.com>
2023-07-07 16:47:50 +0530

    authorization added

--- API/Authentication/Models/AuthenticateResponse.cs
+++ API/Authentication/Models/AuthenticateResponse.cs
@@ -3,5 +3,13 @@ namespace AuthenticationAPI.Models
     public class AuthenticateResponse
     {
         public string? Token { get; set; }   
+
+
+        public AuthenticateResponse(string token){
+            this.Token=token;        
     }
+    }
+
+
+    
 }
--- API/UsersManagement/Helpers/AuthorizeAttribute.cs
+++ API/UsersManagement/Helpers/AuthorizeAttribute.cs
@@ -2,7 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 
-namespace AuthenticationAPI.Helpers
+namespace UsersManagement.Helpers
 {
 
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
