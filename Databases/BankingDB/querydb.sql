1700441d3f64eb85ca4a8f84d53d2f3e65fb7eec
omkar3221 <sudrikbhairavnath@gmail.com>
2023-09-08 19:17:48 +0530

    omkar changes

--- Databases/BankingDB/querydb.sql
+++ Databases/BankingDB/querydb.sql
@@ -1,8 +1,9 @@
 
-SELECT * FROM customers;
+
 select * from accounts;
 SELECT @transactionId;
 SELECT * FROM operations;
+
 SELECT * FROM transactions;
 SELECT * FROM accounts;
 CALL fundtransfer("39025546601","39025546612","MAHB0000286" ,"BARBO0000286",1000,@transactionId);
