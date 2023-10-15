ed488448def08a1d58e7e582604a607b024bdbab
sahilmankar <sahilmankar311@gmail.com>
2023-10-15 22:16:14 +0530

    sample Corporate added

--- Databases/CorporateDB/sampledb.sql
+++ Databases/CorporateDB/sampledb.sql
@@ -1,4 +1,4 @@
--- Active: 1678339848098@@127.0.0.1@3306@corporatedb
+-- Active: 1678359546568@@127.0.0.1@3306@corporatedb
 INSERT INTO corporations VALUES(DEFAULT,"OM Hundekari","90898976655","OmH@gmail.com",4);
 INSERT INTO corporations VALUES(DEFAULT,"Victory Theatre","8978900090","VictoryT@gmail.com",5);
 INSERT INTO corporations VALUES(DEFAULT,"Vikas Theatre","9090090908","VikasT@gmail.com",6); 
@@ -10,5 +10,7 @@ INSERT INTO corporations VALUES(DEFAULT,"Hemantkumar vegetables","9090909090","h
 INSERT INTO corporations VALUES(DEFAULT,"thorat Company","9089898989","hemant123@gmail.com",12);
 INSERT INTO corporations VALUES(DEFAULT,"Kale Company","8909090909","hemant123@gmail.com",13);
 INSERT INTO corporations VALUES(DEFAULT,"Totre Company","9889898989","hemant123@gmail.com",14);
+INSERT INTO corporations VALUES(DEFAULT,"Dhanuka Pvt. Ltd. Company","9873456790","dhanuka@gmail.com",12);
+INSERT INTO corporations VALUES(DEFAULT,"BAYER Pvt. Ltd. Company","9873456792","bayer@gmail.com",13);
 
 
