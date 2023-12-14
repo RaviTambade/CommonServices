57bb27b725c8069f34210c648b7f0e32c81cf4a0
unknown <shubhangi.tambade@gmail.com>
2023-12-14 14:34:48 +0530

    UI updated

--- API/Banking/Repositories/LoanApplicants.cs
+++ API/Banking/Repositories/LoanApplicants.cs
@@ -72,7 +72,7 @@ public class LoanApplicantsRepo : ILoanApplicantRepo
         return status;
     }
 
-    public async Task <bool> Update(LoanaplicantsInfo applicant)
+    public async Task <bool> UpdateStatus(LoanaplicantsInfo applicant)
     {
        
         Console.WriteLine("ApplayDate In Repo:- " + applicant.ApplyDate);
