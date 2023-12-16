3d0a3b654439b5221eace0dbae9ec69804eb68d9
sahilmankar <sahilmankar311@gmail.com>
2023-12-16 10:17:05 +0530

    change in notification service:

--- API/NotificationService/Services/EmailSender.cs
+++ API/NotificationService/Services/EmailSender.cs
@@ -1,3 +1,5 @@
+// using System.Net.Mail;
+using System.Net;
 using MailKit.Net.Smtp;
 using Microsoft.Extensions.Options;
 using MimeKit;
@@ -18,18 +20,57 @@ public class EmailSender : IEmailSender
 
     public async Task SendEmail(EmailMessage message)
     {
-        var emailMessage = CreateEmailMessage(message);
+        var emailMessage = await CreateEmailMessage(message);
 
         await Send(emailMessage);
     }
 
-    private MimeMessage CreateEmailMessage(EmailMessage message)
+    private async Task<MimeMessage> CreateEmailMessage(EmailMessage message)
     {
         var emailMessage = new MimeMessage();
         emailMessage.From.Add(new MailboxAddress("TFLPORTAL", _emailConfig.From));
-        emailMessage.To.AddRange(message.To.Select(x => new MailboxAddress("", x)));
+        emailMessage.To.AddRange(message.To.Select(x => new MailboxAddress(x, x)));
         emailMessage.Subject = message.Subject;
-        emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Text) { Text = message.Body };
+
+        Multipart multipart = new Multipart("mixed");
+
+        TextPart body = new TextPart(MimeKit.Text.TextFormat.Text) { Text = message.Body };
+
+//sending file present on local server 
+        // MimePart attachment = new MimePart("application", "octet-stream")
+        // {
+        //     Content = new MimeContent(System.IO.File.OpenRead("sm.txt")),
+        //     ContentDisposition = new ContentDisposition(ContentDisposition.Attachment),
+        //     ContentTransferEncoding = ContentEncoding.Base64,
+        //     FileName = "sm.txt"
+        // };
+
+//sending file which on remote serve
+
+        // byte[] fileBytes = Array.Empty<byte>();
+        // using (var client = new HttpClient())
+        // {
+        //     var response = await client.GetAsync("http://localhost:5263/abc.txt");
+        //     if (response.IsSuccessStatusCode)
+        //     {
+        //         // Read the file content from the response
+        //         fileBytes = await response.Content.ReadAsByteArrayAsync();
+        //     }
+        // }
+
+        // var attachment = new MimePart("application", "octet-stream")
+        // {
+        //     Content = new MimeContent(new MemoryStream(fileBytes))
+        //     ContentDisposition = new ContentDisposition(ContentDisposition.Attachment),
+        //     ContentTransferEncoding = ContentEncoding.Base64,
+        //     FileName = "abc.txt",
+        // };
+
+        // multipart.Add(attachment);
+
+        multipart.Add(body);
+
+        emailMessage.Body = multipart;
 
         return emailMessage;
     }
