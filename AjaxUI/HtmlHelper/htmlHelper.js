865e4bcf9d14d9903ee43a29a4960d989324b182
RohitM4926 <rohitmangale4926@gmail.com>
2023-12-26 09:16:06 +0530

    Get EMI UI Updated

--- AjaxUI/HtmlHelper/htmlHelper.js
+++ AjaxUI/HtmlHelper/htmlHelper.js
@@ -4,7 +4,7 @@
     {
         console.log(details);
         var htmlContent = '<div class="table-responsive"> <table id="loanapplicantsTable" class="table table-striped"> <thead> <tr>' +
-                            '<th>AccountId</th><th>Applicant Name</th><th padding-left:10px>Application Date</th><th>Loan Amount</th><th>Loan Duration</th><th>Loan Type</th><th>Loan Status</th></tr></thead><body>';
+                            '<th>AccountId</th><th>Applicant Name</th><th padding-left:10px>Application Date</th><th>Loan Amount</th><th>Loan Duration</th><th>Loan Type</th><th>Loan Status</th><th>Your Monthly EMI</th></tr></thead><body>';
                             
                                     htmlContent += '<tr>';
                                     htmlContent +='<td>'+details.accountId + '</td>';
@@ -15,8 +15,9 @@
                                     htmlContent += '<td >'+details.loanTypeName  + '</td>';                                       
                                     htmlContent += '<td>'+details.loanStatus + '</td>';
 
-                                   
-                                    htmlContent += '<td>' + "  " + '<button onclick = "checkstatus(status1,responsedata)" id="btnapproved" class="btn btn-success">Approved</button>' + "  "+ '<button  onclick = "checkstatus(status2,responsedata)" id="btnrejected" class="btn btn-danger">Rejected</button>' + '</td>';
+                                    htmlContent += '<td>' + "  " + '<button onclick = "GetEMI(responsedata)" id="btngetemi" class="btn btn-success">Get EMI </button> ' ;
+                                    htmlContent += '</td>';
+                                    htmlContent += '<td>' + "  " + '<button onclick = "checkstatus(status1,responsedata)" id="btnapproved" class="btn btn-success">Approve </button>' + "    "+ '<button  onclick = "checkstatus(status2,responsedata)" id="btnrejected" class="btn btn-danger">Reject</button>' + '</td>';
                                     htmlContent += '</tr>';
                             
                                 htmlContent += '</tbody></table></div>';
