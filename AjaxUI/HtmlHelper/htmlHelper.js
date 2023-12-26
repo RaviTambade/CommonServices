28aec7053a95c7478af33df643d30cc9b3ceb5f8
unknown <shubhangi.tambade@gmail.com>
2023-12-26 12:33:27 +0530

    loan status

--- AjaxUI/HtmlHelper/htmlHelper.js
+++ AjaxUI/HtmlHelper/htmlHelper.js
@@ -1,10 +1,10 @@
 
 
- function displayData(details)
+ function displayData(details,emi)
     {
         console.log(details);
         var htmlContent = '<div class="table-responsive"> <table id="loanapplicantsTable" class="table table-striped"> <thead> <tr>' +
-                            '<th>AccountId</th><th>Applicant Name</th><th padding-left:10px>Application Date</th><th>Loan Amount</th><th>Loan Duration</th><th>Loan Type</th><th>Loan Status</th></tr></thead><body>';
+                            '<th>AccountId</th><th>Applicant Name</th><th padding-left:10px>Application Date</th><th>Loan Amount</th><th>Loan Duration</th><th>Loan Type</th><th>Loan Status</th> <th>EMI Amount</th></tr></thead><body>';
                             
                                     htmlContent += '<tr>';
                                     htmlContent +='<td>'+details.accountId + '</td>';
@@ -14,10 +14,15 @@
                                     htmlContent += '<td>'+details.loanDuration + '</td>';                                                                      
                                     htmlContent += '<td >'+details.loanTypeName  + '</td>';                                       
                                     htmlContent += '<td>'+details.loanStatus + '</td>';
+                                    htmlContent += '<td>'+ emi + '</td>';
+
 
                                    
-                                    htmlContent += '<td>' + "  " + '<button onclick = "checkstatus(status1,responsedata)" id="btnapproved" class="btn btn-success">Approved</button>' + "  "+ '<button  onclick = "checkstatus(status2,responsedata)" id="btnrejected" class="btn btn-danger">Rejected</button>' + '</td>';
+                                    // htmlContent += '<td>' + "  " + '<button onclick = "checkstatus(status1,responsedata)" id="btnapproved" class="btn btn-success">Approved</button>' + "  "+ '<button  onclick = "checkstatus(status2,responsedata)" id="btnrejected" class="btn btn-danger">Rejected</button>' + '</td>';
+                                    htmlContent += '<td>' + "  " + '<button onclick = "updateLoanAndLoanstatus(status1,details,loanData)" id="btnapproved" class="btn btn-success">Approved</button>' + "  "+ '<button  onclick = "updateLoanAndLoanstatus(status2,details,loandata)" id="btnrejected" class="btn btn-danger">Rejected</button>' + '</td>';
                                     htmlContent += '</tr>';
+
+
                             
                                 htmlContent += '</tbody></table></div>';
                                 $('#dataDisplay').html(htmlContent);
