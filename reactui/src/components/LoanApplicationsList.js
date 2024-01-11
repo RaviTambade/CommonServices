40a5d3745bcdc13afd95320abd3d2fe131865392
sahilmankar <sahilmankar311@gmail.com>
2024-01-11 11:44:46 +0530

    react component updation

--- reactui/src/components/LoanApplicationsList.js
+++ reactui/src/components/LoanApplicationsList.js
@@ -1,94 +1,78 @@
-import React, { useState,useEffect } from "react"
-import { Button, Table } from 'react-bootstrap';
+import React, { useState, useEffect } from "react";
+import { Table } from "react-bootstrap";
 import axios from "axios";
-import { Link } from 'react-router-dom';
- var updatelist=[];
+import { Link } from "react-router-dom";
 function LoanApplicationsList() {
-    
-    var [Status,setStatus]=useState([]);
-    const [allchecked, setAllChecked] = useState([]);
-   
-    const url = "http://localhost:5053/api/application/applicationAscustomer";
-    const [data, setData] = useState([]);
-    updatelist=[...Status];
-   
+  const url = "http://localhost:5053/api/application/applicationAscustomer";
+  const [data, setData] = useState([]);
+  const [toDate, setToDate] = useState(new Date().toISOString().slice(0,10));
+  const [tempData, setTempData] = useState(["applied"]);
 
-    const handleChange=(e)=>{
-        //setAppliedStatus(value)
-        console.log(e.target.value)
-        
-        if (e.target.checked) {
-            // setAllChecked([...allchecked, e.target.value]);
-            //setStatus(data.filter((a)=>a.loanStatus==e.target.value));
-            updatelist=([...Status,data.filter((a)=>a.loanStatus==e.target.value)])
-            // console.log(Status);
-        } else {
-            // setAllChecked(allchecked.filter((item) => item !== e.target.value));
-            // setStatus(data.filter((item) => item !== e.target.value))
-            updatedlist.splice(Status.indexOf(e.target.value), 1);    
-            // console.log(Status);
-        }
-        setStatus(updatedlist);
-        
+  const handleChange = (e) => {
+    if (e.target.checked) {
+      setTempData([...tempData, e.target.value]);
+    } else {
+      setTempData(tempData.filter((item) => item != e.target.value));
     }
+  };
 
-    const fetchInfo = () => {
-        axios
-            .get(url)
-            .then((res) => {
-                console.log(res);
-                setData(res.data);
-                setStatus(res.data);
-            })
-            .catch((err) => {
-                console.log(err);
-            });
+  const getFilteredData =  data.filter((loan) => tempData.includes(loan.loanStatus) && loan.applicationDate<=toDate);
 
-    };
-    useEffect(() => {
-        fetchInfo();
-    }, []);
 
-    return (
-        <div>
-            <h1>Loan Applicnts List</h1>
-            <input value="applied" type="checkbox" onChange={handleChange} />
-            <span>Applied</span>
-            <input value="approved" type="checkbox" onChange={handleChange} />
-            <span>Approved</span>
-            <input value="rejected" type="checkbox" onChange={handleChange} />
-            <span>Rejected</span>
-            <Table striped bordered hover size="sm" >
-                <thead>
-                    <tr>
-                        <th>Sr.No</th>
-                        <th>AccountId</th>
-                        <th>Applicant Name</th>
-                        <th>Loan Status</th>
-                        <th></th>
-                    </tr>
-                </thead>
-                {Status.map((applications, index) => (
+  const fetchInfo = () => {
+    axios
+      .get(url)
+      .then((res) => {
+        console.log(res);
+        setData(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  useEffect(() => {
+    fetchInfo();
+  }, []);
 
-                    <tbody key={applications.applicationId}>
-
-                        <tr >
-                            <td>{index + 1}</td>
-                            <td>{applications.accountId}</td>
-                            <td>{applications.applicantName}</td>
-                            <td>{applications.loanStatus}</td>
-                            {/* <td><button type="submit" onClick={navigateToDetails(applications.applicationId)}>Details</button></td> */}
-                            <td><Link to={`/detailsfromlist/${applications.applicationId}`}>Details</Link></td>
-                        </tr>
-                        {/* <Routes>
-                            <Route key={applications.applicationId} path="/details:key" element={<GetApplicantDetails />} />
-                        </Routes> */}
-                    </tbody>
-                ))}
-            </Table>
-        </div>
-    );
+  return (
+    <div>
+      <h1>Loan Applicnts List</h1>
+      <input type="date" onChange={(e)=>setToDate(e.target.value)}  value={toDate} /> ToDate
+      <input value="applied" type="checkbox"   onChange={handleChange}  defaultChecked/>
+      <span>Applied</span>
+      <input value="approved" type="checkbox" onChange={handleChange} />
+      <span>Approved</span>
+      <input value="rejected" type="checkbox" onChange={handleChange} />
+      <span>Rejected</span>
+      <Table striped bordered hover size="sm">
+        <thead>
+          <tr>
+            <th>Sr.No</th>
+            <th>AccountId</th>
+            <th>Applicant Name</th>
+            <th>Applicant Date</th>
+            <th>Loan filteredList</th>
+            <th></th>
+          </tr>
+        </thead>
+        {getFilteredData.map((applications, index) => (
+          <tbody key={applications.applicationId}>
+            <tr>
+              <td>{index + 1}</td>
+              <td>{applications.accountId}</td>
+              <td>{applications.applicantName}</td>
+              <td>{applications.applicationDate}</td>
+              <td>{applications.loanStatus}</td>
+              <td>
+                <Link to={`/detailsfromlist/${applications.applicationId}`}>
+                  Details
+                </Link>
+              </td>
+            </tr>
+          </tbody>
+        ))}
+      </Table>
+    </div>
+  );
 }
-export default LoanApplicationsList
-
-
+export default LoanApplicationsList;
