c89f6083be317568d9458d888f5a2ea0f702b72b
RohitM4926 <rohitmangale4926@gmail.com>
2024-01-11 12:10:31 +0530

    by dated need to be update

--- reactui/src/components/LoanApplicationsList.js
+++ reactui/src/components/LoanApplicationsList.js
@@ -2,34 +2,27 @@ import React, { useState,useEffect } from "react"
 import { Button, Table } from 'react-bootstrap';
 import axios from "axios";
 import { Link } from 'react-router-dom';
- var updatelist=[];
+
 function LoanApplicationsList() {
     
-    var [Status,setStatus]=useState([]);
-    const [allchecked, setAllChecked] = useState([]);
+    var [Status,setStatus]=useState([]);   
    
     const url = "http://localhost:5053/api/application/applicationAscustomer";
-    const [data, setData] = useState([]);
-    updatelist=[...Status];
+    const [data, setData] = useState([]); 
+    
    
-
     const handleChange=(e)=>{
         //setAppliedStatus(value)
         console.log(e.target.value)
         
         if (e.target.checked) {
             // setAllChecked([...allchecked, e.target.value]);
-            //setStatus(data.filter((a)=>a.loanStatus==e.target.value));
-            updatelist=([...Status,data.filter((a)=>a.loanStatus==e.target.value)])
-            // console.log(Status);
+            setStatus(data.filter((items)=>items.loanStatus==e.target.value));
+                       
         } else {
             // setAllChecked(allchecked.filter((item) => item !== e.target.value));
-            // setStatus(data.filter((item) => item !== e.target.value))
-            updatedlist.splice(Status.indexOf(e.target.value), 1);    
-            // console.log(Status);
-        }
-        setStatus(updatedlist);
-        
+            setStatus(data.filter((item) => item !== e.target.value))          
+        }      
     }
 
     const fetchInfo = () => {
@@ -58,7 +51,7 @@ function LoanApplicationsList() {
             <span>Approved</span>
             <input value="rejected" type="checkbox" onChange={handleChange} />
             <span>Rejected</span>
-            <Table striped bordered hover size="sm" >
+            <Table striped bordered hover  >
                 <thead>
                     <tr>
                         <th>Sr.No</th>
@@ -76,13 +69,10 @@ function LoanApplicationsList() {
                             <td>{index + 1}</td>
                             <td>{applications.accountId}</td>
                             <td>{applications.applicantName}</td>
-                            <td>{applications.loanStatus}</td>
-                            {/* <td><button type="submit" onClick={navigateToDetails(applications.applicationId)}>Details</button></td> */}
+                            <td>{applications.loanStatus}</td>                           
                             <td><Link to={`/detailsfromlist/${applications.applicationId}`}>Details</Link></td>
                         </tr>
-                        {/* <Routes>
-                            <Route key={applications.applicationId} path="/details:key" element={<GetApplicantDetails />} />
-                        </Routes> */}
+                        
                     </tbody>
                 ))}
             </Table>
