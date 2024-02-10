fba9648453c0d71ae962593abe8dcec6e645d1e3
sahilmankar <sahilmankar311@gmail.com>
2024-02-10 11:48:18 +0530

    userDetails updated

--- API/MembershipRolesMgmt/Repositories/Interfaces/IUserRepository.cs
+++ API/MembershipRolesMgmt/Repositories/Interfaces/IUserRepository.cs
@@ -1,5 +1,6 @@
 using Transflower.MembershipRolesMgmt.Models.Entities;
 using Transflower.MembershipRolesMgmt.Models.Requests;
+using Transflower.MembershipRolesMgmt.Models.Responses;
 
 namespace Transflower.MembershipRolesMgmt.Repositories.Interfaces;
 
@@ -10,6 +11,7 @@ public interface IUserRepository
     Task<List<User>> GetUsers();
     Task<List<User>> GetUsersByRole(string roleName);
     Task<List<User>> GetUsersByUserIds(string userIds);
+    Task<List<UserDetails>> GetUserDetailsByUserIds(string userIds);
     Task<User> GetUser(int userId);
     Task<User> GetUser(string contactNumber);
     Task<bool> Update(int id, User user);
