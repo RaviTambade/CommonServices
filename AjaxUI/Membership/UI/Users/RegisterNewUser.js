d2480f7c05bb8cfabf85fa562ad1f543f7c4ccbb
Ajinkya <ajinkyatambade143@gmail.com>
2024-07-03 00:02:18 +0530

    HTML & JS Updated

---
+++ AjaxUI/Membership/UI/Users/RegisterNewUser.js
@@ -0,0 +1,47 @@
+$(document).ready(function() {
+    $('#frm_applicant').submit(function(event) {
+        event.preventDefault(); // Prevent default form submission
+
+        // Get values from form fields
+        var imageUrl = $('#imgurl').val();
+        var aadharId = $('#aadharid').val();
+        var firstName = $('#fname').val();
+        var lastName = $('#lname').val();
+        var birthDate = $('#birthdate').val();
+        var gender = $('#gender').val();
+        var email = $('#email').val();
+        var contact = $('#contact').val();
+        var password = $('#password').val();
+
+        // Create data object to send via AJAX
+        var newData = {
+            "imageUrl": imageUrl,
+            "firstName": firstName,
+            "lastName": lastName,
+            "birthDate": birthDate,
+            "aadharId": aadharId,
+            "gender": gender,
+            "email": email,
+            "contact": contact,
+            "password": password
+        };
+
+        console.log(JSON.stringify(newData));
+
+        // AJAX POST request to insert new data
+        $.ajax({
+            url: 'http://localhost:5000/api/users/add',
+            type: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify(newData),
+            success: function(data) {
+                console.log('Data inserted:', data);
+                alert("Successfully inserted...");
+            },
+            error: function(xhr, status, error) {
+                console.error('Error:', status + ': ' + error);
+                alert("Not inserted...");
+            }
+        });
+    });
+});
---
+++ AjaxUI/Membership/UI/Users/ShowAllUsers.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+    $.ajax({
+        url: 'http://localhost:5000/api/users',
+        type: 'GET',
+        contentType: 'application/json',
+        success: function(response) {
+            var htmlContent = '<div class="table-responsive"><table id="usersTable" class="table table-striped"><thead><tr>' +
+                '<th>Id</th><th>Image URL</th><th>First Name</th><th>Last Name</th><th>Birth Date</th><th>Aadhar Id</th>' +
+                '<th>Gender</th><th>Email</th><th>Contact Number</th><th>Password</th><th>Created On</th><th>Modified On</th>' +
+                '</tr></thead><tbody>';
+
+            $.each(response, function(index, item) {
+                htmlContent += '<tr>' +
+                    '<td>' + item.id + '</td>' +
+                    '<td>' + item.imageUrl + '</td>' +
+                    '<td>' + item.firstName + '</td>' +
+                    '<td>' + item.lastName + '</td>' +
+                    '<td>' + item.birthDate + '</td>' +
+                    '<td>' + item.aadharId + '</td>' +
+                    '<td>' + item.gender + '</td>' +
+                    '<td>' + item.email + '</td>' +
+                    '<td>' + item.contactNumber + '</td>' +
+                    '<td>' + item.password + '</td>' +
+                    '<td>' + item.createdOn + '</td>' +
+                    '<td>' + item.modifiedOn + '</td>' +
+                    '</tr>';
+            });
+
+            htmlContent += '</tbody></table></div>';
+            $('#dataDisplay').html(htmlContent);
+        },
+        error: function(xhr, status, error) {
+            console.error('Error:', status + ': ' + error);
+        }
+    });
+});
