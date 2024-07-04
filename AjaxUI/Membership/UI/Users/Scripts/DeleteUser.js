a90f105e53ac2b57534a77e210404007a43503db
Nayankothawade <nayankothawade133@gmail.com>
2024-07-04 13:09:31 +0530

    Alert Delete Messege

--- AjaxUI/Membership/UI/Users/Scripts/DeleteUser.js
+++ AjaxUI/Membership/UI/Users/Scripts/DeleteUser.js
@@ -2,26 +2,27 @@ document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("btndeleteuser").addEventListener("click", function () {
         var userId = document.getElementById("txtUserId").value;
 
-        fetch('http://localhost:5000/api/users/' +userId, {
+        fetch('http://localhost:5000/api/users/' + userId, {
             method: 'DELETE',
             headers: {
                 'Content-Type': 'application/json'
             }
         })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.json();
-        })
-        .then(data => {
-        
-            console.log('User deleted successfully:', data);
-         
-        })
-        .catch(error => {
-        
-            console.error('Error:', error);
-        });
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
+            .then(data => {
+
+                alert("User Deleted Successfully");
+                console.log('User deleted successfully:', data);
+
+            })
+            .catch(error => {
+
+                console.error('Error:', error);
+            });
     });
 });
