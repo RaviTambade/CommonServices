8fd003dbb7592d49263c77e2492e2f7f99398e33
shubhangtambade <shubhangi.tambade@gmail.com>
2024-07-08 11:17:12 +0530

    UserProfile_Navigation

--- AjaxUI/Membership/UI/Navigation/Scripts/Login.js
+++ AjaxUI/Membership/UI/Navigation/Scripts/Login.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
     $("#btnsubmit").click(function () {    
       var contactNo = $("#contactNo").val();
+      console.log(contactNo);
       var pass = $("#pass").val();
       var lob = $("#lob").val();
 
@@ -19,5 +20,29 @@ $(document).ready(function() {
           console.error(xhr.responseText);
         }
       });
+
+      $.ajax({
+        url: "http://localhost:5000/api/users/contact/"+ contactNo,
+        type: 'GET',
+        contentType: 'application/json',
+        success: function (data) {
+          console.log("Use's Data : "+ data.id);
+          //const Userdata = JSON.parse(data);
+          //console.log(Userdata);
+          window.sessionStorage.setItem("userid", data.id);
+         // window.localStorage.setItem("userid",data);
+
+         jQuery( document ).ajaxSuccess(function( event, xhr, settings ) {
+          window.location.href = 'http://127.0.0.1:5500/Membership/UI/Navigation/UserProfile.html';
+        });
+        
+          //localStorage.setItem('token', data.token); // Assuming 'token' is returned from API
+        },
+        error: function (xhr, status, error) {
+          console.error(xhr.responseText);
+        }
+      });
+     
     });
+
   });
---
+++ AjaxUI/Membership/UI/Navigation/Scripts/UserProfile.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+
+    var userId = parseInt(sessionStorage.getItem("userid"));
+        console.log("User Id = " +userId);
+        //$("#txtUserId").text = userId;
+        document.getElementById("uid").innerHTML = userId;
+
+    $("#btnShow").click(function () {
+       // var userId = $("#txtUserId").val();
+      
+        $.ajax({
+            url: "http://localhost:5000/api/users/userdetails/" + userId,
+            type: 'GET',
+            contentType: 'application/json',
+
+            success: function (data) {
+                $("#pid").text(data.id);
+                $("#pfname").text(data.firstName);
+                $("#plname").text(data.lastName);
+                $("#pemail").text(data.email);
+                $("#paadharid").text(data.aadharId);
+                $("#pdob").text(data.birthDate);
+                $("#pcontact").text(data.contactNumber);
+                $("#pgender").text(data.gender);
+                $("#pimgurl").text(data.imageUrl);
+                $("#pcreatedon").text(data.createdOn);
+                $("#pmodifiedon").text(data.modifiedOn);
+            },
+            error: function (xhr, status, error) {
+                console.error(xhr.responseText);
+            }
+        });
+    });
+});
