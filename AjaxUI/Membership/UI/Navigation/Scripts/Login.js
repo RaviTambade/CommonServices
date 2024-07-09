b49ab472618d6d46642fe6045963181b539899c6
shubhangtambade <shubhangi.tambade@gmail.com>
2024-07-09 12:00:26 +0530

    Lob dropdownlist

--- AjaxUI/Membership/UI/Navigation/Scripts/Login.js
+++ AjaxUI/Membership/UI/Navigation/Scripts/Login.js
@@ -1,7 +1,48 @@
 $(document).ready(function() {
+   var rolesInfo;
+   var userId;
 
+   $("#contactNo").change(function(){
+    
+      var contactNo = $("#contactNo").val();
+      console.log("Contact " + contactNo);
+      $.ajax({
+        url: "http://localhost:5000/api/users/contact/" + contactNo,
+        type: 'GET',
+        contentType: 'application/json',
+        success: function (userData) {
+          console.log("User's Data:", userData.id);
+          window.sessionStorage.setItem("userid", userData.id);
+          
+        },
+          error: function (xhr, status, error) {
+            console.error("Error authenticating user:", xhr.responseText);
+           
+          }
+          
+        });
+        
+         userId = window.sessionStorage.getItem("userid");
+        $.ajax({
+          url: "http://localhost:5000/api/roles/" + userId,
+          type: 'GET',
+          contentType: 'application/json',
+          success: function (rolesData) {
+            console.log("Roles:", rolesData);
+            
+
+          },
+          error: function (xhr, status, error) {
+            console.error("Error fetching roles:", xhr.responseText);
+          }
+        });
+
+   });
+   
+
+   //After click on Sign In button
   $("#btnsubmit").click(function () {
-    var contactNo = $("#contactNo").val();
+    //var contactNo = $("#contactNo").val();
     var pass = $("#pass").val();
     var lob = $("#lob").val();
 
@@ -38,17 +79,22 @@ $(document).ready(function() {
               contentType: 'application/json',
               success: function (rolesData) {
                 console.log("Roles:", rolesData);
-               
+                
+
                 rolesData.forEach(function(role) {
                   console.log("Role:", role.name);
                   // Redirect based on role name
-                  if (role.name === "Director") 
+                  if (role.name === "Director" ) 
                   {
-                    window.location.href = 'http://127.0.0.1:5500/AjaxUI/Membership/UI/Navigation/DirectorDashboard.html';
+                    window.location.href = 'http://127.0.0.1:5500/Membership/UI/Navigation/DirectorDashboard.html';
                   } 
-                  else if (role.name === "HR Manager") 
+                  else if (role.name === "HR Manager" ) 
                   {
-                    window.location.href = 'http://127.0.0.1:5500/AjaxUI/Membership/UI/Navigation/ManagerDashboard.html';
+                    window.location.href = 'http://127.0.0.1:5500/Membership/UI/Navigation/ManagerDashboard.html';
+                  }
+                  else
+                  {
+                    window.location.href = 'http://127.0.0.1:5500/Membership/UI/Navigation/UserProfile.html';
                   }
                 });
               },
@@ -68,5 +114,5 @@ $(document).ready(function() {
       }
     });
   });
-
+  
 });
