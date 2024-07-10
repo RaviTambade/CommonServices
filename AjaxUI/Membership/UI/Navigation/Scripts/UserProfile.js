d8924590707007baa8eaeb84f7be984c094aebb6
Ajinkya <ajinkyatambade143@gmail.com>
2024-07-10 22:38:14 +0530

    Pages Modified

--- AjaxUI/Membership/UI/Navigation/Scripts/UserProfile.js
+++ AjaxUI/Membership/UI/Navigation/Scripts/UserProfile.js
@@ -2,33 +2,36 @@ $(document).ready(function () {
     console.log("Inside userProfile.js");
     var userId = parseInt(sessionStorage.getItem("userid"));
     console.log("User Id = " + userId);
-    //$("#txtUserId").text = userId;
     document.getElementById("uid").innerHTML = userId;
 
-    $("#btnShow").click(function () {
-        // var userId = $("#txtUserId").val();
+    $.ajax({
+        url: "http://localhost:5000/api/users/userdetails/" + userId,
+        type: 'GET',
+        contentType: 'application/json',
 
-        $.ajax({
-            url: "http://localhost:5000/api/users/userdetails/" + userId,
-            type: 'GET',
-            contentType: 'application/json',
+        success: function (data) {
+            $("#profilePhoto").attr("src", data.imageUrl); // Update profile photo
+            $("#userName").text(data.firstName + " " + data.lastName);
+            $("#userEmail").text(data.email);
+            $("#userRole").text(data.role); // Assuming you have a role field in your data
 
-            success: function (data) {
-                $("#pfname").text(data.firstName);
-                $("#plname").text(data.lastName);
-                $("#pemail").text(data.email);
-                $("#paadharid").text(data.aadharId);
-                $("#pdob").text(data.birthDate);
-                $("#pcontact").text(data.contactNumber);
-                $("#pgender").text(data.gender);
-                // $("#pid").text(data.id);
-                // $("#pimgurl").text(data.imageUrl);
-                // $("#pcreatedon").text(data.createdOn);
-                // $("#pmodifiedon").text(data.modifiedOn);
-            },
-            error: function (xhr, status, error) {
-                console.error(xhr.responseText);
-            }
-        });
+            $("#pfname").text(data.firstName);
+            $("#plname").text(data.lastName);
+            $("#pemail").text(data.email);
+            $("#paadharid").text(data.aadharId);
+            $("#pdob").text(data.birthDate);
+            $("#pcontact").text(data.contactNumber);
+            $("#pgender").text(data.gender);
+            $("#pid").text(data.id);
+            // $("#pcreatedon").text(data.createdOn);
+            // $("#pmodifiedon").text(data.modifiedOn);
+        },
+        error: function (xhr, status, error) {
+            console.error(xhr.responseText);
+        }
+    });
+
+    $("#btnShowDetails").click(function () {
+        $("#additionalDetails").toggleClass("hidden");
     });
 });
