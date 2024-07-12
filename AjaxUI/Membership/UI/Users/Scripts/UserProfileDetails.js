375284cb1aacde45ebd4cc08ab85f496a9d6660c
Bhupebdra5454 <akshayw2512@gmail.com>
2024-07-12 16:54:37 +0530

    User Profile

--- AjaxUI/Membership/UI/Users/Scripts/UserProfileDetails.js
+++ AjaxUI/Membership/UI/Users/Scripts/UserProfileDetails.js
@@ -1,25 +1,26 @@
 $(document).ready(function () {
    
-        var userId = parseInt(sessionStorage.getItem("userid")); 
-        $.ajax({
-            url: "http://localhost:5000/api/users/userdetails/" + userId,
-            type: 'GET',
-            contentType: 'application/json',
+    var userId = parseInt(sessionStorage.getItem("userid")); 
+    $.ajax({
+        url: "http://localhost:5000/api/users/userdetails/" + userId,
+        type: 'GET',
+        contentType: 'application/json',
 
-            success: function (data) {
-                $("#pfname").text(data.firstName);
-                $("#plname").text(data.lastName);
-                $("#pemail").text(data.email);
-                $("#paadharid").text(data.aadharId);
-                $("#pdob").text(data.birthDate);
-                $("#pcontact").text(data.contactNumber);
-                $("#pgender").text(data.gender);
-                $("#pimgurl").attr("src",data.imageUrl);
-                $("#pcreatedon").text(data.createdOn);
-                $("#pmodifiedon").text(data.modifiedOn);
-            },
-            error: function (xhr, status, error) {
-                console.error(xhr.responseText);
-            }
-        });
+        success: function (data) {
+            var fullname = data.firstName+" "+data.lastName;
+            $("#fullname").text(fullname);
+            $("#pemail").text(data.email);
+            $("#paadharid").text(data.aadharId);
+            $("#pdob").text(data.birthDate);
+            $("#pcontact").text(data.contactNumber);
+            $("#pgender").text(data.gender);
+            $("#pimgurl").attr("src",data.imageUrl);
+            $("#pcreatedon").text(data.createdOn);
+            $("#pmodifiedon").text(data.modifiedOn);
+        },
+        error: function (xhr, status, error) {
+            console.error(xhr.responseText);
+        }
     });
+});
+
