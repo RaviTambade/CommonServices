30d887be9469478f8931dd341b95bc6335ef4c9d
Nayankothawade <nayankothawade133@gmail.com>
2024-07-16 17:49:46 +0530

    Get User Acc. to Role

--- AjaxUI/Membership/UI/Navigation/Scripts/TryUserRoleById.js
+++ AjaxUI/Membership/UI/Navigation/Scripts/TryUserRoleById.js
@@ -17,7 +17,7 @@ document.addEventListener("DOMContentLoaded", function () {
                         radiobox.type = 'radio';
                         radiobox.id = 'num';
                         radiobox.name = "options";
-                        radiobox.value = item.id;
+                        radiobox.value = item.name;
                         var label = document.createElement('label')
                         label.htmlFor = 'num';
                         var description = document.createTextNode(item.name);
@@ -25,22 +25,35 @@ document.addEventListener("DOMContentLoaded", function () {
                         rolesContainer.appendChild(radiobox);
                         rolesContainer.appendChild(label);
                     });
-                    // var radioButtons = document.querySelectorAll('input[type="radio"]');
+                    var radioButtons = document.querySelectorAll('input[type="radio"]');
                     radioButtons.forEach(function (radio) {
                         radio.addEventListener('click', function () {
                             if (this.checked) {
                                 var selectedOption = this.value;
                                 console.log("Selected Option: " + selectedOption);
-                                console.log("Selected LOB"+ userLOB);
+                                console.log("Selected LOB: " + userLOB);
                                 $.ajax({
                                     url: `http://localhost:5000/api/roles/getuserbyroles/rolename/${selectedOption}/lob/${userLOB}`,
                                     type: 'GET',
                                     contentType: 'application/json',
                                     success: function (data) {
-                                        console.log(data);
+                                        var htmlContent = '<div class="overflow-x-auto"><table id="userByRoleTable" class="min-w-full divide-y divide-gray-200"><thead class="bg-gray-50"><tr>' +
+                                            '<th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Id</th>' +
+                                            '<th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Image</th>' +
+                                            '<th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">First Name</th>' +
+                                            '<th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Last Name</th>' +
+                                            '</tr></thead><tbody class="bg-white divide-y divide-gray-200">';
 
-                                        // const roles = $("#roles").empty();
-                                       // data.forEach(role => roles.append($('<div></div>').text(role.name)));
+                                        $.each(data, function (index, item) {
+                                            htmlContent += '<tr>' +
+                                                '<td class="px-6 py-4 whitespace-nowrap">' + item.id + '</td>' +
+                                                '<td class="px-6 py-4 whitespace-nowrap">' + item.imageUrl + '</td>' +
+                                                '<td class="px-6 py-4 whitespace-nowrap">' + item.firstName + '</td>' +
+                                                '<td class="px-6 py-4 whitespace-nowrap">' + item.lastName + '</td>' +
+                                                '</tr>';
+                                        });
+                                        htmlContent += '</tbody></table></div>';
+                                        $('#dataDisplay').html(htmlContent);
                                     },
                                     error: function (xhr, status, error) {
                                         console.error(xhr.responseText);
