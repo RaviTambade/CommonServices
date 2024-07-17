11434fc95196824f1039a658817c43597f00325f
Ajinkya <ajinkyatambade143@gmail.com>
2024-07-17 17:22:45 +0530

    Modifications

--- Solutions/UI/classicui/Membership/UI/Navigation/dashboard/scripts/Director.js
+++ Solutions/UI/classicui/Membership/UI/Navigation/dashboard/scripts/Director.js
@@ -2,25 +2,28 @@ $(document).ready(function () {
     console.log("Inside Director DashBoard.js");
     var userId = parseInt(sessionStorage.getItem("userid"));
 
-        console.log("User Id = " +userId);
-        $("#uid").text = userId;
-       
-        //document.getElementById("uid").innerHTML = userId;
+    console.log("User Id = " + userId);
+    $("#uid").text(userId);
 
-        $.ajax({
-            url: "http://localhost:5000/api/users/userdetails/" + userId,
-            type: 'GET',
-            contentType: 'application/json',
-
-            success: function (data) {
-               console.log(data);
-               $("#fname").text(data.firstName);
-               $("#lname").text(data.lastName);
-                
-            },
-            error: function (xhr, status, error) {
-                console.error(xhr.responseText);
-            }
-        });
-
+    $.ajax({
+        url: "http://localhost:5000/api/users/userdetails/" + userId,
+        type: 'GET',
+        contentType: 'application/json',
+        success: function (data) {
+            console.log(data);
+            $("#fname").text(data.firstName);
+            $("#lname").text(data.lastName);
+            $("#user-name").text(data.firstName + " " + data.lastName);
+        },
+        error: function (xhr, status, error) {
+            console.error(xhr.responseText);
+        }
     });
+
+    const dropdownButton = $('.relative button');
+    const dropdownMenu = $('.relative .absolute');
+
+    dropdownButton.on('click', function () {
+        dropdownMenu.toggleClass('hidden');
+    });
+});
