9530aebf9effe55c425c8da2eec88ac512b3d0b8
Nayankothawade <nayankothawade133@gmail.com>
2024-07-17 16:57:01 +0530

    Fixing Path

--- Solutions/UI/classicui/Membership/UI/Users/Scripts/SearchUserByContact.js
+++ Solutions/UI/classicui/Membership/UI/Users/Scripts/SearchUserByContact.js
@@ -1,16 +1,15 @@
-$(document).ready(function() {
-    $('#getUserDetailsBtn').on('click', function() {
+$(document).ready(function () {
+    $('#getUserDetailsBtn').on('click', function () {
         getUserdetailsByContact();
     });
 });
-
 function getUserdetailsByContact() {
     var userContact = $('#contact').val();
     $.ajax({
         url: 'http://localhost:5000/api/users/contact/' + userContact,
         type: 'GET',
         contentType: 'application/json',
-        success: function(response) {
+        success: function (response) {
             var htmlContent = '<div class="overflow-x-auto"><table id="userByContactTable" class="min-w-full divide-y divide-gray-200"><thead class="bg-gray-50"><tr>' +
                 '<th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Id</th>' +
                 '<th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Image URL</th>' +
@@ -24,7 +23,6 @@ function getUserdetailsByContact() {
                 '<th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Created On</th>' +
                 '<th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Modified On</th>' +
                 '</tr></thead><tbody class="bg-white divide-y divide-gray-200">';
-
             htmlContent += '<tr>' +
                 '<td class="px-6 py-4 whitespace-nowrap">' + response.id + '</td>' +
                 '<td class="px-6 py-4 whitespace-nowrap">' + response.imageUrl + '</td>' +
@@ -38,11 +36,10 @@ function getUserdetailsByContact() {
                 '<td class="px-6 py-4 whitespace-nowrap">' + response.createdOn + '</td>' +
                 '<td class="px-6 py-4 whitespace-nowrap">' + response.modifiedOn + '</td>' +
                 '</tr>';
-
             htmlContent += '</tbody></table></div>';
             $('#dataDisplay').html(htmlContent);
         },
-        error: function(xhr, status, error) {
+        error: function (xhr, status, error) {
             console.error('Error:', status, error);
             alert("Failed to fetch user details.");
         }
